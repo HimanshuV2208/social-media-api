69b9c19dcd052512d3a628aee52a08029d437115
Himanshu <him2282001@gmail.com>
2024-03-24 15:19:18 +0530

    Create User model and a DAO service that works on static List of users

---
+++ src/main/java/com/himanshu/rest/webservices/socialmediaapi/service/UserDaoService.java
@@ -0,0 +1,24 @@
+package com.himanshu.rest.webservices.socialmediaapi.service;
+
+import com.himanshu.rest.webservices.socialmediaapi.models.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Bob", LocalDate.now().minusYears(25)));
+        users.add(new User(3, "Charlie", LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+}
