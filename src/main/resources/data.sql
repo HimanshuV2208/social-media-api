391779f777c0e0098c0c78f6a305f74c05c159d2
Himanshu <him2282001@gmail.com>
2024-04-03 14:33:25 +0530

    Enable the H2 database and add dummy user details into the database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user_details(id, birth_date, name)
+values(101, current_date(), 'Himanshu1');
+insert into user_details(id, birth_date, name)
+values(102, current_date(), 'Himanshu2');
+insert into user_details(id, birth_date, name)
+values(103, current_date(), 'Himanshu3');
